db856927917a70c7fe0e258272dbe635ad4918b5
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-01-19 16:05:09 +0530

    Makes the folder where the database is stored

--- createdatabase.py
+++ createdatabase.py
@@ -1,6 +1,9 @@
+import os
 import sqlite3
 
 def main():
+    if not os.path.exists('Databases'):
+        os.makedirs('Databases')
     cnx = sqlite3.connect('Databases\\snekscores.db')
     csr = cnx.cursor()
     csr.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
