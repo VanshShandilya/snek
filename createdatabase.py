837d9efcd70051303927cc1b5a28bdad14e6761d
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-01-23 13:45:20 +0530

    Update createdatabase.py

--- createdatabase.py
+++ createdatabase.py
@@ -4,7 +4,7 @@ import sqlite3
 def main():
     if not os.path.exists('Databases'):
         os.makedirs('Databases')
-    cnx = sqlite3.connect('Databases\\snekscores.db')
+    cnx = sqlite3.connect('./Databases/snekscores.db')
     csr = cnx.cursor()
     csr.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
     data = csr.fetchall()
