9a19493f5519bd3bb4242a085d56216e569e30e3
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-01-24 21:34:17 +0530

    Update createdatabase.py

--- createdatabase.py
+++ createdatabase.py
@@ -6,11 +6,12 @@ def main():
         os.makedirs('Databases')
     cnx = sqlite3.connect('./Databases/snekscores.db')
     csr = cnx.cursor()
-    csr.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
+    csr.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
     data = csr.fetchall()
     if ('snekscores',) not in data:
         csr.execute("CREATE TABLE snekscores(id integer primary key autoincrement, Username varchar(255), highScore integer);")
         print('Table successfully made')
+    cnx.close()
 
 if __name__ == '__main__':
     main()
